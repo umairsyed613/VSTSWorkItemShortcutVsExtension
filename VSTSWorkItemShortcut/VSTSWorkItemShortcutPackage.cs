5e9167198c52b41806eb805c66a93b608057b082
Umair Hussein <umahus@melinmedical.com>
2019-08-27 11:38:52 +0200

    ccc

--- VSTSWorkItemShortcut/VSTSWorkItemShortcutPackage.cs
+++ VSTSWorkItemShortcut/VSTSWorkItemShortcutPackage.cs
@@ -25,6 +25,7 @@ namespace VSTSWorkItemShortcut
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(VSTSWorkItemShortcutPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class VSTSWorkItemShortcutPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace VSTSWorkItemShortcut
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await Command.InitializeAsync(this);
         }
 
         #endregion
